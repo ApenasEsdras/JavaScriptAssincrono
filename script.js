939ee381f6e807abc01a88975004c4d3d40c9bf4
ApenasEsdras <apenasesdras@gmail.com>
2022-03-21 20:38:06 -0300

    colocacaoDoHTMLCSSJSatualizados

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-const myPromise = new Promise((resolve, reject) => {
-    window.setTimeout(() => {
-        resolve('Resolvida');
-    }, 2000);
-});
+// Introduçao aos mecanismos
 
-await myPromise
-    .then((result) => result + 'passando pelo then')
-    .then((result) => result + 'e agora acobou!')
-    .catch((err) => console.log(err.message));
+// const myPromise = new Promise((resolve, reject) => {
+//     window.setTimeout(() => {
+//         resolve('Resolvida');
+//     }, 2000);
+// });
+
+// await myPromise
+//     .then((result) => result + 'passando pelo then')
+//     .then((result) => result + 'e agora acobou!')
+//     .catch((err) => console.log(err.message));
+
+// projeto do catcast 
+
+
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const catBtn = document.getElementById('change-cat');
+
+
+const getCats = async () => {
+    try {
+        const data = fetch(BASE_URL);
+        const json = await (await data).json();
+        return json.webpurl;
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+const loadImg = async () => {
+    const catImg = document.getElementById('cat');
+    catImg.src = await getCats();
+};
+
+catBtn.addEventListener('click', loadImg);
+
+loadImg();
