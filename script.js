53255325b1e5487d1ea5c2e880eb6f27da4dff41
ApenasEsdras <apenasesdras@gmail.com>
2022-03-21 20:01:18 -0300

    PrimeiroCommit

---
+++ script.js
@@ -0,0 +1,10 @@
+const myPromise = new Promise((resolve, reject) => {
+    window.setTimeout(() => {
+        resolve('Resolvida');
+    }, 2000);
+});
+
+await myPromise
+    .then((result) => result + 'passando pelo then')
+    .then((result) => result + 'e agora acobou!')
+    .catch((err) => console.log(err.message));
