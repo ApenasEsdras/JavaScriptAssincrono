e60fcc8c8fefe55c2d0cd04bbb65d7fe3692f83e
ApenasEsdras <apenasesdras@gmail.com>
2022-03-22 14:27:57 -0300

    Junção das aulas

---
+++ Map, Filter e Reduce/map.js
@@ -0,0 +1,28 @@
+// Pratique a sintaxe de multiplicação de números, uma vez utilizando o parâmetro this de um objeto criado por você, e depois sem ele.
+
+const orange = {
+    price: 2,
+};
+
+const apple = {
+    price: 1.5,
+};
+
+function mapArray() {
+    const array = [1, 2, 3, 4, 5];
+
+    return array.map(function (item) {
+        return item * this.price;
+    }, apple);
+}
+
+console.log(mapArray());
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>...
+
+const array = [1, 2, 3, 4, 5];
+
+console.log(array.map((item) => item * 2));
+
+console.log(array);
